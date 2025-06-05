0378d5c95458a283d92dc649b9636ebcac821a1e
rpalcab <rpalcab@gmail.com>
2025-06-05 13:29:33 +0200

    added RVD to plot, improved aesthetics. close #23

--- bin/circos_plot.py
+++ bin/circos_plot.py
@@ -8,6 +8,7 @@ Part of PITISfinder. Processes /tag qualifiers in features (if any)
 from pycirclize import Circos
 from pycirclize.parser import Genbank
 import numpy as np
+import math
 from matplotlib.patches import Patch
 from matplotlib.lines import Line2D
 import argparse
@@ -27,6 +28,12 @@ def get_args() -> argparse.Namespace:
     )
 
     parser.add_argument(
+        '-m', '--mge_elements',
+        action="store_true",
+        help="Plot MGE elements (integrase, MPF, MOB...)"
+    )
+
+    parser.add_argument(
         '-o', '--output', required=True,
         type=Path,
         help='Required. Final output png file'
@@ -42,70 +49,125 @@ def main():
     seqid2size = gbk.get_seqid2size()
     space = 0 if len(seqid2size) == 1 else 2
     circos = Circos(sectors=seqid2size, space=space)
-    circos.text(f"{gbk.name}", size=12, r=20)
+    circos.text(f"{gbk.name}", size=12, r=150)
 
     seqid2features = gbk.get_seqid2features(feature_type=None)
     seqid2seq = gbk.get_seqid2seq()
+
+    feature_presence = {
+        "Forward CDS": False,
+        "Reverse CDS": False,
+        "rRNA": False,
+        "tRNA": False,
+        "Plasmid": False,
+        "Integron": False,
+        "Prophage": False,
+        "IS": False,
+        "AMR": False,
+        "VF": False,
+        "DF": False,
+        "GC Content": True,
+        "GC Skew": True,
+    }
+
+    track_info = [
+        ("cds_track", (92, 98), "CDS"),
+        ("rna_track", (89, 92), "rRNA/tRNA"),
+        ("pl_track", (86, 89), "Plasmid"),
+        ("int_track", (83, 86), "Integron"),
+        ("ph_track", (80, 83), "Prophage"),
+        ("is_track", (77, 80), "IS"),
+        ("rvd_track", (74, 77), "AMR/VF/DF")
+    ]
+
     for sector in circos.sectors:
-        # Plot outer track with xticks
-        major_ticks_interval = 500000
-        minor_ticks_interval = 100000
+
         outer_track = sector.add_track((98, 100))
         outer_track.axis(fc="lightgrey")
-        if sector.size >= major_ticks_interval:
-            outer_track.xticks_by_interval(
-                major_ticks_interval, label_formatter=lambda v: f"{v/ 10 ** 6:.1f} Mb"
-            )
-            outer_track.xticks_by_interval(minor_ticks_interval, tick_length=1, show_label=False)
+        # GC tracks
+        gc_content_track = sector.add_track((65, 70), r_pad_ratio=0.1)
+        gc_skew_track = sector.add_track((55, 65), r_pad_ratio=0.1)
+        tracks = {'outer_track': outer_track,
+                  'gc_content_track': gc_content_track,
+                  'gc_skew_track': gc_skew_track
+                  }
 
-        f_cds_track = sector.add_track((90, 97), r_pad_ratio=0.1)
-        r_cds_track = sector.add_track((83, 90), r_pad_ratio=0.1)
-        # rrna_track = sector.add_track((76, 83), r_pad_ratio=0.1)
-        # trna_track = sector.add_track((76, 83), r_pad_ratio=0.1)
-        amr_track = sector.add_track((76, 83), r_pad_ratio=0.1)
-        pl_track = sector.add_track((73, 76), r_pad_ratio=0.1)
-        int_track = sector.add_track((70, 73), r_pad_ratio=0.1)
-        ph_track = sector.add_track((67, 70), r_pad_ratio=0.1)
-        is_track = sector.add_track((64, 67), r_pad_ratio=0.1)
+        for i, (track_var, radius_range, label) in enumerate(track_info):
+            bg_color = "#f8f8f8" if (i % 2 == 0) else "#eaeaea"
+            track = sector.add_track(radius_range, r_pad_ratio=0.1)
+            track.axis(fc=bg_color, ec="lightgrey", lw=0.3)
+            tracks[track_var] = track
 
-        # Plot Forward CDS, Reverse CDS, rRNA, tRNA
+        # Plot CDS (fwd, rev) rRNA, tRNA and MGEs
         features = seqid2features[sector.name]
         for feature in features:
+            # Gral features
             if feature.type == "CDS" and feature.location.strand == 1:
-                f_cds_track.genomic_features(feature, fc="red")
+                tracks["cds_track"].genomic_features(feature, plotstyle="arrow", fc="#0082C8")
+                feature_presence["Forward CDS"] = True
             elif feature.type == "CDS" and feature.location.strand == -1:
-                r_cds_track.genomic_features(feature, fc="blue")
-            # elif feature.type == "rRNA":
-            #     rrna_track.genomic_features(feature, fc="green")
-            # if feature.type == "tRNA":
-            #     trna_track.genomic_features(feature, color="magenta", lw=0.1)
+                tracks["cds_track"].genomic_features(feature, plotstyle="arrow", fc="#E6194B")
+                feature_presence["Reverse CDS"] = True
+            elif feature.type == "rRNA":
+                tracks["rna_track"].genomic_features(feature, fc="#3CB44B")
+                feature_presence["rRNA"] = True
+            elif feature.type == "tRNA":
+                tracks["rna_track"].genomic_features(feature, color="#FFE119", lw=0.1)
+                feature_presence["tRNA"] = True
+
+            # MGEs
+            elif feature.type == "MGE" and 'plasmid' in feature.qualifiers.get('type', []):
+                tracks["pl_track"].genomic_features(feature, color="#911EB4", lw=0.1)
+                feature_presence["Plasmid"] = True
+            elif feature.type == "MGE" and 'integron' in feature.qualifiers.get('type', []):
+                tracks["int_track"].genomic_features(feature, color="#F58230", lw=0.1)
+                feature_presence["Integron"] = True
+            elif feature.type == "MGE" and 'prophage' in feature.qualifiers.get('type', []):
+                tracks["ph_track"].genomic_features(feature, color="#46F0F0", lw=0.1)
+                feature_presence["Prophage"] = True
+            elif feature.type == "MGE" and 'IS' in feature.qualifiers.get('type', []):
+                tracks["is_track"].genomic_features(feature, color="#F032E6", lw=0.1)
+                feature_presence["IS"] = True
+
+            # AMR, VF, DF
             if feature.type == "CDS" and 'AMR' in feature.qualifiers.get('tag', []):
-                amr_track.genomic_features(feature, color="cyan", lw=0.1)
-            if feature.type == "MGE" and 'plasmid' in feature.qualifiers.get('type', []):
-                pl_track.genomic_features(feature, color="green", lw=0.1)
-            if feature.type == "MGE" and 'integron' in feature.qualifiers.get('type', []):
-                int_track.genomic_features(feature, color="magenta", lw=0.1)
-            if feature.type == "MGE" and 'prophage' in feature.qualifiers.get('type', []):
-                ph_track.genomic_features(feature, color="grey", lw=0.1)
-            if feature.type == "MGE" and 'IS' in feature.qualifiers.get('type', []):
-                is_track.genomic_features(feature, color="purple", lw=0.1)
+                tracks["rvd_track"].genomic_features(feature, color="#D2F53C", lw=0.1)
+                feature_presence["AMR"] = True
+            if feature.type == "CDS" and 'VF' in feature.qualifiers.get('tag', []):
+                tracks["rvd_track"].genomic_features(feature, color="#AA6E28", lw=0.1)
+                feature_presence["VF"] = True
+            if feature.type == "CDS" and 'DF' in feature.qualifiers.get('tag', []):
+                tracks["rvd_track"].genomic_features(feature, color="#808080", lw=0.1)
+                feature_presence["DF"] = True
 
         # Plot 'gene' qualifier label if exists
         labels, label_pos_list = [], []
         for feature in features:
-            start = int(feature.location.start)
-            end = int(feature.location.end)
+            start, end = int(feature.location.start), int(feature.location.end)
             label_pos = (start + end) / 2
             gene_name = feature.qualifiers.get("gene", [None])[0]
-            if gene_name is not None and 'AMR' in feature.qualifiers.get('tag', []):
+            if gene_name is not None and any(tag in feature.qualifiers.get('tag', []) for tag in ['AMR', 'VF', 'DF']):
+                tracks["cds_track"].annotate(label_pos, gene_name, label_size=7, text_kws = {"weight":"bold"})
                 labels.append(gene_name)
                 label_pos_list.append(label_pos)
-        f_cds_track.xticks(label_pos_list, labels, label_size=6, label_orientation="vertical")
 
-        # # Plot xticks (interval = 10 Kb)
-        # r_cds_track.xticks_by_interval(
-        #     10000, outer=False, label_formatter=lambda v: f"{v/1000:.1f} Kb"
-        # )
+        # Plot xticks & intervals on inner position
+        genome_size = sum(gbk.get_seqid2size().values())
+        if genome_size > 1000000:
+            interval = math.ceil((genome_size * .97 // 8) / 100000) * 100000
+            div=1000000
+            units="Mb"
+        else:
+            interval = math.ceil((genome_size // 8) / 1000) * 1000
+            div=1000
+            units="Kb"
+        tracks["cds_track"].xticks_by_interval(
+            interval=interval,
+            outer=True,
+            label_formatter=lambda v: f"{v/ div:.1f} {units}",
+            label_orientation="vertical",
+            line_kws=dict(ec="grey"),
+        )
 
         # Plot GC content
         gc_content_track = sector.add_track((55, 60))
@@ -140,23 +202,38 @@ def main():
 
     fig = circos.plotfig()
     # Add legend
-    handles = [
-        Patch(color="red", label="Forward CDS"),
-        Patch(color="blue", label="Reverse CDS"),
-        # Patch(color="green", label="rRNA"),
-        # Patch(color="magenta", label="tRNA"),
-        Patch(color="cyan", label="AMR"),
-        Patch(color="green", label="Plasmid"),
-        Patch(color="magenta", label="Integron"),
-        Patch(color="grey", label="Prophage"),
-        Patch(color="purple", label="IS"),
+    handles = []
 
-        Line2D([], [], color="black", label="Positive GC Content", marker="^", ms=6, ls="None"),
-        Line2D([], [], color="grey", label="Negative GC Content", marker="v", ms=6, ls="None"),
-        Line2D([], [], color="olive", label="Positive GC Skew", marker="^", ms=6, ls="None"),
-        Line2D([], [], color="purple", label="Negative GC Skew", marker="v", ms=6, ls="None"),
-    ]
-    _ = circos.ax.legend(handles=handles, bbox_to_anchor=(0.5, 0.475), loc="center", fontsize=8)
+    legend_map = {
+        "Forward CDS": Patch(color="#E6194B", label="Forward CDS"),
+        "Reverse CDS": Patch(color="#0082C8", label="Reverse CDS"),
+        "rRNA": Patch(color="#3CB44B", label="rRNA"),
+        "tRNA": Patch(color="#FFE119", label="tRNA"),
+        "Plasmid": Patch(color="#911EB4", label="Plasmid"),
+        "Integron": Patch(color="#F58230", label="Integron"),
+        "Prophage": Patch(color="#46F0F0", label="Prophage"),
+        "IS": Patch(color="#F032E6", label="IS"),
+        "AMR": Patch(color="#D2F53C", label="AMR"),
+        "VF": Patch(color="#AA6E28", label="VF"),
+        "DF": Patch(color="#808080", label="DF"),
+        "GC Content": [
+            Line2D([], [], color="black", label="Positive GC Content", marker="^", ms=6, ls="None"),
+            Line2D([], [], color="grey", label="Negative GC Content", marker="v", ms=6, ls="None"),
+        ],
+        "GC Skew": [
+            Line2D([], [], color="olive", label="Positive GC Skew", marker="^", ms=6, ls="None"),
+            Line2D([], [], color="purple", label="Negative GC Skew", marker="v", ms=6, ls="None"),
+        ],
+    }
+
+    for key, entry in legend_map.items():
+        if feature_presence[key]:
+            if isinstance(entry, list):
+                handles.extend(entry)
+            else:
+                handles.append(entry)
+
+    circos.ax.legend(handles=handles, bbox_to_anchor=(0.5, 0.5), loc="center", fontsize=8)
 
     fig.savefig(args.output)
 
